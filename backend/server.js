ad836a5004a9e921725cbb3af0ff3134f94d9e14
Emmanuel <emmanuel.martinez@ucsp.edu.pe>
2025-10-05 01:09:57 -0500

    asdasd

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// rutas
 app.use("/users", userRoutes);
 
-// documentación
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
   console.log(`📖 Swagger en http://localhost:${PORT}/api-docs`);
 });
